322362dc483cb2d0b620d57c9b7e778602bc5fbc
ntzwilly <ntzwilly@gmail.com>
2023-05-17 19:04:22 +0200

    Example 2

--- decorators.js
+++ decorators.js
@@ -15,7 +15,49 @@ const countCalls = (fn) => {
 
 sum = countCalls(sum)
 
-console.log(sum(10, 11))
-console.log(sum(10, 16, 12, 78))
-console.log(sum(17, 11, 35))
+// console.log(sum(10, 11))
+// console.log(sum(10, 16, 12, 78))
+// console.log(sum(17, 11, 35))
+
+/**
+ * *Example of using decorator to validate data
+ */
+
+let rectangleArea = (length, width) => length * width;
+
+const countArgs = (fn) => {
+  return (...args) => {
+    try{
+      if(args.length !== fn.length) {
+        throw new Error(`Incorrect number of arguments for ${fn.name}`);
+      }
+    }catch(error) {
+      console.log(error.message)  
+    }
+    return fn(...args)
+  }
+}
+
+const requireIntegers = (fn) => {
+  let name = fn.name
+  return (name, ...args) => {
+    try {
+      args.forEach(arg => {
+        if(!Number.isInteger(arg)) {
+          throw new TypeError(`Arguments for ${name} must be integers`)
+        }
+      })
+    } catch (error) {
+      console.log(error.message)
+    };
+    return fn(...args)
+  }
+}
+
+rectangleArea = countArgs(rectangleArea)
+rectangleArea = requireIntegers(rectangleArea)
+console.log(rectangleArea(12, 'hello'))
+// console.log(rectangleArea(12, 10, 15))
+// console.log(rectangleArea(12, 13))
+
 
